907541cee9ec845c210068eca0b52d8e9c2f6c5a
Ray Silva <silvaray355@gmail.com>
2024-10-10 13:13:27 -0300

    Improving contacts

--- Frontend/Program.cs
+++ Frontend/Program.cs
@@ -4,8 +4,10 @@ using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
+
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
+
 var apiUrl = builder.Configuration["API_URL"];
 var client = new HttpClient { BaseAddress = new Uri(apiUrl ?? "http://localhost:5156") };
 
@@ -13,4 +15,5 @@ builder.Services.AddScoped(sp => client);
 builder.Services.AddBlazorBootstrap();
 builder.Services.AddTransient<HttpService>();
 builder.Services.AddTransient<TokenService>();
+
 await builder.Build().RunAsync();
